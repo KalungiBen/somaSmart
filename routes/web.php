0e00da29647c65d85183ac71c2e69c7b4ea3a9a0
Soeng Souy <soengsouy.khmer09@gmail.com>
2023-10-09 18:44:39 +0700

    add page invoice list

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\ForgotPasswordController;
-use App\Http\Controllers\ResetPasswordController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\UserManagementController;
-use App\Http\Controllers\TypeFormController;
-use App\Http\Controllers\Setting;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\TeacherController;
-use App\Http\Controllers\DepartmentController;
-use App\Http\Controllers\SubjectController;
-use App\Http\Controllers\InvoiceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -51,79 +38,84 @@ Route::group(['middleware'=>'auth'],function()
 });
 
 Auth::routes();
+Route::group(['namespace' => 'App\Http\Controllers\Auth'],function()
+{
+    // ----------------------------login ------------------------------//
+    Route::controller(LoginController::class)->group(function () {
+        Route::get('/login', 'login')->name('login');
+        Route::post('/login', 'authenticate');
+        Route::get('/logout', 'logout')->name('logout');
+        Route::post('change/password', 'changePassword')->name('change/password');
+    });
 
-// ----------------------------login ------------------------------//
-Route::controller(LoginController::class)->group(function () {
-    Route::get('/login', 'login')->name('login');
-    Route::post('/login', 'authenticate');
-    Route::get('/logout', 'logout')->name('logout');
-    Route::post('change/password', 'changePassword')->name('change/password');
+    // ----------------------------- register -------------------------//
+    Route::controller(RegisterController::class)->group(function () {
+        Route::get('/register', 'register')->name('register');
+        Route::post('/register','storeUser')->name('register');    
+    });
 });
 
-// ----------------------------- register -------------------------//
-Route::controller(RegisterController::class)->group(function () {
-    Route::get('/register', 'register')->name('register');
-    Route::post('/register','storeUser')->name('register');    
-});
+Route::group(['namespace' => 'App\Http\Controllers'],function()
+{
+    // -------------------------- main dashboard ----------------------//
+    Route::controller(HomeController::class)->group(function () {
+        Route::get('/home', 'index')->middleware('auth')->name('home');
+        Route::get('user/profile/page', 'userProfile')->middleware('auth')->name('user/profile/page');
+        Route::get('teacher/dashboard', 'teacherDashboardIndex')->middleware('auth')->name('teacher/dashboard');
+        Route::get('student/dashboard', 'studentDashboardIndex')->middleware('auth')->name('student/dashboard');
+    });
 
-// -------------------------- main dashboard ----------------------//
-Route::controller(HomeController::class)->group(function () {
-    Route::get('/home', 'index')->middleware('auth')->name('home');
-    Route::get('user/profile/page', 'userProfile')->middleware('auth')->name('user/profile/page');
-    Route::get('teacher/dashboard', 'teacherDashboardIndex')->middleware('auth')->name('teacher/dashboard');
-    Route::get('student/dashboard', 'studentDashboardIndex')->middleware('auth')->name('student/dashboard');
-});
+    // ----------------------------- user controller ---------------------//
+    Route::controller(UserManagementController::class)->group(function () {
+        Route::get('list/users', 'index')->middleware('auth')->name('list/users');
+        Route::post('change/password', 'changePassword')->name('change/password');
+        Route::get('view/user/edit/{id}', 'userView')->middleware('auth');
+        Route::post('user/update', 'userUpdate')->name('user/update');
+        Route::post('user/delete', 'userDelete')->name('user/delete');
+    });
 
-// ----------------------------- user controller -------------------------//
-Route::controller(UserManagementController::class)->group(function () {
-    Route::get('list/users', 'index')->middleware('auth')->name('list/users');
-    Route::post('change/password', 'changePassword')->name('change/password');
-    Route::get('view/user/edit/{id}', 'userView')->middleware('auth');
-    Route::post('user/update', 'userUpdate')->name('user/update');
-    Route::post('user/delete', 'userDelete')->name('user/delete');
-});
+    // ------------------------ setting -------------------------------//
+    Route::controller(Setting::class)->group(function () {
+        Route::get('setting/page', 'index')->middleware('auth')->name('setting/page');
+    });
 
-// ------------------------ setting -------------------------------//
-Route::controller(Setting::class)->group(function () {
-    Route::get('setting/page', 'index')->middleware('auth')->name('setting/page');
-});
+    // ------------------------ student -------------------------------//
+    Route::controller(StudentController::class)->group(function () {
+        Route::get('student/list', 'student')->middleware('auth')->name('student/list'); // list student
+        Route::get('student/grid', 'studentGrid')->middleware('auth')->name('student/grid'); // grid student
+        Route::get('student/add/page', 'studentAdd')->middleware('auth')->name('student/add/page'); // page student
+        Route::post('student/add/save', 'studentSave')->name('student/add/save'); // save record student
+        Route::get('student/edit/{id}', 'studentEdit'); // view for edit
+        Route::post('student/update', 'studentUpdate')->name('student/update'); // update record student
+        Route::post('student/delete', 'studentDelete')->name('student/delete'); // delete record student
+        Route::get('student/profile/{id}', 'studentProfile')->middleware('auth'); // profile student
+    });
 
-// ------------------------ student -------------------------------//
-Route::controller(StudentController::class)->group(function () {
-    Route::get('student/list', 'student')->middleware('auth')->name('student/list'); // list student
-    Route::get('student/grid', 'studentGrid')->middleware('auth')->name('student/grid'); // grid student
-    Route::get('student/add/page', 'studentAdd')->middleware('auth')->name('student/add/page'); // page student
-    Route::post('student/add/save', 'studentSave')->name('student/add/save'); // save record student
-    Route::get('student/edit/{id}', 'studentEdit'); // view for edit
-    Route::post('student/update', 'studentUpdate')->name('student/update'); // update record student
-    Route::post('student/delete', 'studentDelete')->name('student/delete'); // delete record student
-    Route::get('student/profile/{id}', 'studentProfile')->middleware('auth'); // profile student
-});
+    // ------------------------ teacher -------------------------------//
+    Route::controller(TeacherController::class)->group(function () {
+        Route::get('teacher/add/page', 'teacherAdd')->middleware('auth')->name('teacher/add/page'); // page teacher
+        Route::get('teacher/list/page', 'teacherList')->middleware('auth')->name('teacher/list/page'); // page teacher
+        Route::get('teacher/grid/page', 'teacherGrid')->middleware('auth')->name('teacher/grid/page'); // page grid teacher
+        Route::post('teacher/save', 'saveRecord')->middleware('auth')->name('teacher/save'); // save record
+        Route::get('teacher/edit/{id}', 'editRecord'); // view teacher record
+        Route::post('teacher/update', 'updateRecordTeacher')->middleware('auth')->name('teacher/update'); // update record
+        Route::post('teacher/delete', 'teacherDelete')->name('teacher/delete'); // delete record teacher
+    });
 
-// ------------------------ teacher -------------------------------//
-Route::controller(TeacherController::class)->group(function () {
-    Route::get('teacher/add/page', 'teacherAdd')->middleware('auth')->name('teacher/add/page'); // page teacher
-    Route::get('teacher/list/page', 'teacherList')->middleware('auth')->name('teacher/list/page'); // page teacher
-    Route::get('teacher/grid/page', 'teacherGrid')->middleware('auth')->name('teacher/grid/page'); // page grid teacher
-    Route::post('teacher/save', 'saveRecord')->middleware('auth')->name('teacher/save'); // save record
-    Route::get('teacher/edit/{id}', 'editRecord'); // view teacher record
-    Route::post('teacher/update', 'updateRecordTeacher')->middleware('auth')->name('teacher/update'); // update record
-    Route::post('teacher/delete', 'teacherDelete')->name('teacher/delete'); // delete record teacher
-});
+    // ----------------------- department -----------------------------//
+    Route::controller(DepartmentController::class)->group(function () {
+        Route::get('department/list/page', 'departmentList')->middleware('auth')->name('department/list/page'); // department/list/page
+        Route::get('department/add/page', 'indexDepartment')->middleware('auth')->name('department/add/page'); // page add department
+        Route::get('department/edit/page', 'editDepartment')->middleware('auth')->name('department/edit/page'); // page add department
+    });
 
-// ----------------------- department -----------------------------//
-Route::controller(DepartmentController::class)->group(function () {
-    Route::get('department/list/page', 'departmentList')->middleware('auth')->name('department/list/page'); // department/list/page
-    Route::get('department/add/page', 'indexDepartment')->middleware('auth')->name('department/add/page'); // page add department
-    Route::get('department/edit/page', 'editDepartment')->middleware('auth')->name('department/edit/page'); // page add department
-});
+    // ----------------------- subject -----------------------------//
+    Route::controller(SubjectController::class)->group(function () {
+        Route::get('subject/list/page', 'subjectList')->middleware('auth')->name('subject/list/page'); // subject/list/page
+    });
 
-// ----------------------- subject -----------------------------//
-Route::controller(SubjectController::class)->group(function () {
-    Route::get('subject/list/page', 'subjectList')->middleware('auth')->name('subject/list/page'); // subject/list/page
-});
-
-// ----------------------- invoice -----------------------------//
-Route::controller(InvoiceController::class)->group(function () {
-    Route::get('invoice/list/page', 'invoiceList')->middleware('auth')->name('invoice/list/page'); // subjeinvoicect/list/page
+    // ----------------------- invoice -----------------------------//
+    Route::controller(InvoiceController::class)->group(function () {
+        Route::get('invoice/list/page', 'invoiceList')->middleware('auth')->name('invoice/list/page'); // subjeinvoicect/list/page
+    });
 });
